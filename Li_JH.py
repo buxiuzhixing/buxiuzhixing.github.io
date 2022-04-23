a383fc7aaf219687ad0e01b90b8b55391445afcd
SeeDream <64675266+buxiuzhixing@users.noreply.github.com>
2022-04-23 11:23:54 +0800

    I ❤️ He LeEr

---
+++ Li_JH.py
@@ -0,0 +1,7 @@
+import os
+import webbrowser as w
+os.system("toilet -f mono12 -F gay 'I AI'")
+os.system("toilet -f mono12 -F gay 'He.LE'")
+print("I ❤️ He.LeEr")
+back=input("Back to buxiuzhixing.github.io")
+w.open(https://buxiuzhixing.github.io/,new=0,autoraise=True)
